3aa492d2575b3c027110f1b276c216848c082143
Jorge Alejandro Marcial Galvan <galvannus@gmail.com>
2019-01-04 23:25:30 -0700

    added unauthenticated view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    def resource
+        #Asigna a @resource User.new si esta basia la vaiable
+        @resource ||= User.new
+    end
+
+    def resource_name
+        :user
+    end
+
+    def resource_class
+        User
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,15 @@ Rails.application.routes.draw do
   resources :accounts
   resources :students
   devise_for :users
-  root 'main#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  authenticated :user do
+    root 'main#index'
+  end
+
+  unauthenticated :user do
+    root 'main#unauthenticated'
+  end
 
   get '/asignar' => 'students#assigment'
   post '/asignar' => 'students#assigment'
